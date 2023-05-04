31eb84d7e91f9ad22214508f7cfd34087cb16308
pySoo <soopy368@gmail.com>
2023-05-04 22:57:39 +0900

    [FEAT] : isFocused에 따라 css 변경

--- src/components/SearchSection/SearchWordBox/SearchWord/index.tsx
+++ src/components/SearchSection/SearchWordBox/SearchWord/index.tsx
@@ -4,14 +4,22 @@ type SearchWordProps = {
   inputText?: string;
   word: string;
   clickWord: (word: string) => void;
+  isFocused: boolean;
 };
 
-function SearchWord({ inputText, word, clickWord }: SearchWordProps) {
+function SearchWord({
+  inputText,
+  word,
+  clickWord,
+  isFocused,
+}: SearchWordProps) {
   return (
     <button
       type="button"
       onClick={() => clickWord(word)}
-      className="flex items-center gap-x-3 px-6 py-2 w-full text-gray-300 hover:bg-gray-50"
+      className={`flex items-center gap-x-3 px-6 py-2 w-full text-gray-300 hover:bg-gray-50${
+        isFocused ? ' bg-gray-50' : ''
+      }`}
     >
       <SearchIcon size="16" />
       <span className="text-black">
