87bcb98fc2627f68e0d7fb6bd52a365b4cfeebb6
pySoo <soopy368@gmail.com>
2023-05-04 22:56:33 +0900

    [FEAT] : 키보드 네비게이션 함수 추가

--- src/components/SearchSection/index.tsx
+++ src/components/SearchSection/index.tsx
@@ -1,21 +1,82 @@
-import { useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import useRecentSearchWords from '@/hooks/useRecentSearchWords';
-import SearchBar from './SearchBar';
-import SearchWordBox from './SearchWordBox';
+import useDebounce from '@/hooks/useDebounce';
+import { searchAPI } from '@/services/search';
+import { MAX_DISPLAYED } from '@/constants/searchWord';
+import SearchBar from './SearchBar/index';
+import SearchWordBox from './SearchWordBox/index';
+import { DEFAULT_INDEX } from '@/constants/config';
 
 function SearchSection() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [inputText, setInputText] = useState('');
   const [isOnFocus, setIsOnFocus] = useState(false);
   const { recentSearchWords, updateRecentSearchWords } = useRecentSearchWords();
+  const [autocompleteWords, setAutocompleteWords] = useState<SearchWordType[]>(
+    [],
+  );
+
+  const [focusIndex, setFocusIndex] = useState<number>(DEFAULT_INDEX);
 
   const wordBoxRef = useRef<HTMLDivElement>(null);
 
-  const search = (searchInputText: string) => {
+  const onSearch = (searchInputText: string) => {
+    if (searchInputText.trim().length === 0) return;
     updateRecentSearchWords(searchInputText);
 
     setIsOnFocus(false);
     if (wordBoxRef.current) wordBoxRef.current.className = 'hidden';
   };
+
+  const debouncedInputText = useDebounce(inputText);
+
+  const handleKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      if (!isOnFocus) return;
+
+      const currentData = autocompleteWords;
+      if (e.key === 'ArrowDown') {
+        e.preventDefault();
+
+        setFocusIndex((currentIndex) =>
+          currentIndex === currentData.length - 1
+            ? currentIndex
+            : currentIndex + 1,
+        );
+      }
+
+      if (e.key === 'ArrowUp') {
+        e.preventDefault();
+        setFocusIndex((currentIndex) =>
+          currentIndex < 1 ? 0 : currentIndex === -1 ? -1 : currentIndex - 1,
+        );
+      }
+
+      if (e.key === 'Enter') {
+        setInputText(currentData[focusIndex].name);
+        setIsOnFocus(false);
+      }
+    },
+    [autocompleteWords, focusIndex],
+  );
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleKeyDown]);
+
+  useEffect(() => {
+    const fetchAutocompleteWords = async () => {
+      setIsLoading(true);
+      const words = await searchAPI(debouncedInputText.trim());
+      setIsLoading(false);
+      setAutocompleteWords(words.slice(0, MAX_DISPLAYED));
+    };
+
+    fetchAutocompleteWords();
+  }, [debouncedInputText]);
 
   return (
     <section className="flex flex-col justify-center items-center pt-20 pb-40 w-full bg-skyblue">
@@ -34,15 +95,18 @@ function SearchSection() {
           inputText={inputText}
           setInputText={setInputText}
           isOnFocus={isOnFocus}
-          search={search}
+          onSearch={onSearch}
         />
 
         <div ref={wordBoxRef} className={isOnFocus ? '' : 'opacity-0'}>
           <SearchWordBox
+            isLoading={isLoading}
             inputText={inputText}
             setInputText={setInputText}
             recentSearchWords={recentSearchWords}
-            search={search}
+            onSearch={onSearch}
+            autocompleteWords={autocompleteWords}
+            focusIndex={focusIndex}
           />
         </div>
       </div>
